4b4676d43dd089905181371ebc09594f817c9b4b
Rochez Benjamin <rochez.benjamin@gmail.com>
2019-07-12 16:15:25 +0200

    Created the threejs scene & added the socket link

--- client/desktop/index.js
+++ client/desktop/index.js
@@ -4,8 +4,46 @@ window.onload = () => {
 
   socket.on("mobile orientation", function(data) {
     if (previousValue !== data) {
-      console.log(data);
+      cube.position.x += data / 100;
     }
     previousValue = data;
   });
 };
+
+let scene, camera, renderer, geometry, material;
+
+scene = new THREE.Scene();
+camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+geometry = new THREE.BoxGeometry(1, 1, 1);
+material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 10;
+
+var animate = function() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+};
+
+animate();
+
+//
+window.addEventListener("resize", onWindowResize, false);
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
